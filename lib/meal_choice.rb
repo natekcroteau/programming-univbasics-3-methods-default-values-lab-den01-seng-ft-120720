3cfead502f14f927090855eba0aed8fb3267892e
Nate Croteau <natekcroteau@gmail.com>
2020-11-12 22:31:03 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,4 +6,6 @@ def meal_choice(veg1, veg2, pro = "tofu")
   puts "A plate of #{pro} with #{veg1} and #{veg2}."
 end
 
+meal_choice("carrots", "string beans", "lentils")
+
 meal_choice("carrots", "string beans")
